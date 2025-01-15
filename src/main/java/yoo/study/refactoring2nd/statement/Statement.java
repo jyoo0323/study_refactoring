5835d4bfad62168a6407faaa154235b0a43069c5
jyoo0323 <githubaccount@email.com>
2025-01-15 23:36:05 +0900

    1.4 statement 함수의 switch 문을 제거하여 amountFor 함수 추출하고 호출하도록 statement 함수 수정.

--- src/main/java/yoo/study/refactoring2nd/statement/Statement.java
+++ src/main/java/yoo/study/refactoring2nd/statement/Statement.java
@@ -16,25 +16,7 @@ public class Statement {
 
 		for (Performance perf : invoice.getPerformances()) {
 			Play play = plays.getPlayById(perf.getPlayID());
-			int thisAmount = 0;
-
-			switch (play.getType()) {
-				case "tragedy":
-					thisAmount = 40000;
-					if (perf.getAudience() > 30) {
-						thisAmount += 1000 * (perf.getAudience() - 30);
-					}
-					break;
-				case "comedy":
-					thisAmount = 30000;
-					if (perf.getAudience() > 20) {
-						thisAmount += 10000 + 500 * (perf.getAudience() - 20);
-					}
-					thisAmount += 300 * perf.getAudience();
-					break;
-				default:
-					throw new RuntimeException("알 수 없는 장르: " + play.getType());
-			}
+			int thisAmount = amountFor(perf, play);
 
 			// 포인트를 적립한다.
 			volumeCredits += Math.max(perf.getAudience() - 30, 0);
@@ -51,5 +33,27 @@ public class Statement {
 		result += String.format("적립 포인트: %d점\n", volumeCredits);
 
 		return result;
+	}
+
+	private int amountFor(Performance perf, Play play) {
+		int thisAmount = 0;
+		switch (play.getType()) {
+			case "tragedy":
+				thisAmount = 40000;
+				if (perf.getAudience() > 30) {
+					thisAmount += 1000 * (perf.getAudience() - 30);
+				}
+				break;
+			case "comedy":
+				thisAmount = 30000;
+				if (perf.getAudience() > 20) {
+					thisAmount += 10000 + 500 * (perf.getAudience() - 20);
+				}
+				thisAmount += 300 * perf.getAudience();
+				break;
+			default:
+				throw new RuntimeException("알 수 없는 장르: " + play.getType());
+		}
+		return thisAmount;
 	}
 }
