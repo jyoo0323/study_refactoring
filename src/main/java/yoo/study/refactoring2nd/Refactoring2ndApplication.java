5dbef78be7771cfdeddb1b1d633a08b8e67bbba7
jyoo0323 <githubaccount@email.com>
2025-01-15 22:19:34 +0900

    initial commit

---
+++ src/main/java/yoo/study/refactoring2nd/Refactoring2ndApplication.java
@@ -0,0 +1,7 @@
+package yoo.study.refactoring2nd;
+
+public class Refactoring2ndApplication {
+
+	public static void main(String[] args) {
+	}
+}
---
+++ src/test/java/yoo/study/refactoring2nd/Refactoring2ndApplicationTests.java
@@ -0,0 +1,13 @@
+package yoo.study.refactoring2nd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Refactoring2ndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
