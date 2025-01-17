5bcf5a09bf4503ba403ccd30fbcceb622289c329
jyoo0323 <githubaccount@email.com>
2025-01-17 23:24:09 +0900

    불필요하게 추가되어 있던 에너테이션 제거

--- src/main/java/yoo/study/refactoring2nd/statement/Invoice.java
+++ src/main/java/yoo/study/refactoring2nd/statement/Invoice.java
@@ -3,13 +3,9 @@ package yoo.study.refactoring2nd.statement;
 import java.io.Serializable;
 import java.util.List;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Invoice implements Serializable {
 	private String customer;
 	private List<Performance> performances;
--- src/main/java/yoo/study/refactoring2nd/statement/Play.java
+++ src/main/java/yoo/study/refactoring2nd/statement/Play.java
@@ -2,13 +2,9 @@ package yoo.study.refactoring2nd.statement;
 
 import java.io.Serializable;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Play implements Serializable {
 	private String name;
 	private String type;
