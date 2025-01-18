35f2f1239c22cd2f26c9de9236b07f0069c735ce
jyoo0323 <githubaccount@email.com>
2025-01-18 16:19:05 +0900

    1.6 totalAmount 와 totalVolume 마운 계산 함수를 StatementData 로 이전.

--- src/main/java/yoo/study/refactoring2nd/statement/Statement.java
+++ src/main/java/yoo/study/refactoring2nd/statement/Statement.java
@@ -2,7 +2,6 @@ package yoo.study.refactoring2nd.statement;
 
 import java.text.NumberFormat;
 import java.util.Currency;
-import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -16,20 +15,15 @@ public class Statement {
 	}
 
 	public String statement() {
-		return renderPlainText(createStatementData(invoice, plays));
-	}
-
-	public StatementData createStatementData(Invoice invoice, Map<String, Play> plays) {
-		StatementData statementData = new StatementData();
-		statementData.setCustomer(invoice.getCustomer());
-		statementData.setPerformances(invoice.getPerformances().stream().map(this::enrichPerformance).toList());
-		statementData.setTotalAmount(totalAmount(statementData.getPerformances()));
-		statementData.setTotalVolumeCredits(totalVolumeCredits(statementData.getPerformances()));
-		return statementData;
-	}
-
-	private EnrichedPerformance enrichPerformance(Performance aPerformance) {
-		return EnrichedPerformance.create(aPerformance, plays.get(aPerformance.getPlayID()));
+		return renderPlainText(
+			StatementData.of(
+				invoice,
+				invoice.getPerformances()
+					.stream()
+					.map(
+						perf -> EnrichedPerformance.create(perf, plays.get(perf.getPlayID()))
+					).toList()
+			));
 	}
 
 	private String renderPlainText(StatementData data) {
@@ -42,14 +36,6 @@ public class Statement {
 		result += String.format("총액: %s\n", usd(data.getTotalAmount()));
 		result += String.format("적립 포인트: %d점\n", data.getTotalVolumeCredits());
 		return result;
-	}
-
-	private int totalAmount(List<EnrichedPerformance> performances) {
-		return performances.stream().reduce(0, (total, perf) -> total + perf.getAmount(), Integer::sum);
-	}
-
-	private int totalVolumeCredits(List<EnrichedPerformance> performances) {
-		return performances.stream().reduce(0, (total, perf) -> total + perf.getVolumeCredits(), Integer::sum);
 	}
 
 	private String usd(int aNumber) {
--- src/main/java/yoo/study/refactoring2nd/statement/StatementData.java
+++ src/main/java/yoo/study/refactoring2nd/statement/StatementData.java
@@ -2,14 +2,31 @@ package yoo.study.refactoring2nd.statement;
 
 import java.util.List;
 
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
+@Builder
 public class StatementData {
 	private String customer;
 	private List<EnrichedPerformance> performances;
 	private int totalAmount;
 	private int totalVolumeCredits;
+
+	public static StatementData of(Invoice invoice, List<EnrichedPerformance> performances) {
+		return StatementData.builder()
+			.customer(invoice.getCustomer())
+			.performances(performances)
+			.totalAmount(totalAmount(performances))
+			.totalVolumeCredits(totalVolumeCredits(performances))
+			.build();
+	}
+
+	private static int totalAmount(List<EnrichedPerformance> performances) {
+		return performances.stream().reduce(0, (total, perf) -> total + perf.getAmount(), Integer::sum);
+	}
+
+	private static int totalVolumeCredits(List<EnrichedPerformance> performances) {
+		return performances.stream().reduce(0, (total, perf) -> total + perf.getVolumeCredits(), Integer::sum);
+	}
 }
