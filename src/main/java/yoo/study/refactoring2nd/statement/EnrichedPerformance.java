247bec6340b148a5c2d26b5272b4ece2d0c835d1
jyoo0323 <githubaccount@email.com>
2025-01-18 16:42:35 +0900

    1.8 PerformanceCalculator amountFor & volumeCreditsFor 함수를 PerformanceCalculator로 이전

--- src/main/java/yoo/study/refactoring2nd/statement/EnrichedPerformance.java
+++ src/main/java/yoo/study/refactoring2nd/statement/EnrichedPerformance.java
@@ -18,39 +18,8 @@ public class EnrichedPerformance {
 			.playId(perf.getPlayID())
 			.play(play)
 			.audience(perf.getAudience())
-			.amount(amountFor(play, perf.getAudience()))
-			.volumeCredits(volumeCreditsFor(play, perf.getAudience()))
+			.amount(calculator.amountFor())
+			.volumeCredits(calculator.volumeCreditsFor())
 			.build();
-	}
-
-	private static int amountFor(Play play, int audience) {
-		int result;
-		switch (play.getType()) {
-			case "tragedy":
-				result = 40000;
-				if (audience > 30) {
-					result += 1000 * (audience - 30);
-				}
-				break;
-			case "comedy":
-				result = 30000;
-				if (audience > 20) {
-					result += 10000 + 500 * (audience - 20);
-				}
-				result += 300 * audience;
-				break;
-			default:
-				throw new RuntimeException("알 수 없는 장르: " + play.getType());
-		}
-		return result;
-	}
-
-	private static int volumeCreditsFor(Play play, int audience) {
-		int result = 0;
-		result += Math.max(audience - 30, 0);
-		if ("comedy".equals(play.getType())) {
-			result += Math.floor(audience / 5);
-		}
-		return result;
 	}
 }
--- src/main/java/yoo/study/refactoring2nd/statement/PerformanceCalculator.java
+++ src/main/java/yoo/study/refactoring2nd/statement/PerformanceCalculator.java
@@ -13,4 +13,35 @@ public class PerformanceCalculator {
 			.play(play)
 			.build();
 	}
+
+	public int amountFor() {
+		int result;
+		switch (play.getType()) {
+			case "tragedy":
+				result = 40000;
+				if (performance.getAudience() > 30) {
+					result += 1000 * (performance.getAudience() - 30);
+				}
+				break;
+			case "comedy":
+				result = 30000;
+				if (performance.getAudience() > 20) {
+					result += 10000 + 500 * (performance.getAudience() - 20);
+				}
+				result += 300 * performance.getAudience();
+				break;
+			default:
+				throw new RuntimeException("알 수 없는 장르: " + play.getType());
+		}
+		return result;
+	}
+
+	public int volumeCreditsFor() {
+		int result = 0;
+		result += Math.max(performance.getAudience() - 30, 0);
+		if ("comedy".equals(play.getType())) {
+			result += Math.floor(performance.getAudience() / 5);
+		}
+		return result;
+	}
 }
